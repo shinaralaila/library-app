1f571c0c818bdc1fe8c0d49c8e95cf47102a3b44
Shinara <shinaralaila@gmail.com>
2021-12-20 11:01:09 +0800

    heroku

--- src/model/userdata.js
+++ src/model/userdata.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/library");
+// mongoose.connect("mongodb://localhost:27017/library");
+mongoose.connect("mongodb+srv://userone:<password>@cluster0.elmop.mongodb.net/test");
 const Schema= mongoose.Schema
 const userschema=new Schema({
     NAME:String,
