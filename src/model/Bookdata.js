660c529242bfa248d79c0aa983fbc3626094bec5
Shinara <shinaralaila@gmail.com>
2021-12-20 13:09:03 +0800

    heroku

--- src/model/Bookdata.js
+++ src/model/Bookdata.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 //mongoose.connect("mongodb://localhost:27017/library");
-mongoose.connect("mongodb+srv://userone:<password>@cluster0.elmop.mongodb.net/test");
+mongoose.connect("mongodb+srv://userone:userone@cluster0.elmop.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority");
 
 const Schema=mongoose.Schema;
 const BookSchema=new Schema({
